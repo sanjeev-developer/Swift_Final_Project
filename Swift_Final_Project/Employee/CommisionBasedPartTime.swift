b54b28f4f918a2632bfda19ea32f71487481887f
sanjeev <sanjeevgupta9223@gmail.com>
2019-10-15 18:44:54 -0400

    create full-time and commission based class

---
+++ Swift_Final_Project/Employee/CommisionBasedPartTime.swift
@@ -0,0 +1,9 @@
+//
+//  CommisionBasedPartTime.swift
+//  Swift_Final_Project
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
