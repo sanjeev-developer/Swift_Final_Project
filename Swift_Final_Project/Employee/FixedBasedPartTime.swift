36c53a8044723bcdd9e87031c3d46972fdfa2905
sanjeev <sanjeevgupta9223@gmail.com>
2019-10-15 19:06:23 -0400

    created remains classes of employee

---
+++ Swift_Final_Project/Employee/FixedBasedPartTime.swift
@@ -0,0 +1,9 @@
+//
+//  FixedBasedPartTime.swift
+//  Swift_Final_Project
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
