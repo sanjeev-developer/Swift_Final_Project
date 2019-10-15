18def9f959c80b043d4d4b64e9557db2bd00fbc9
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-15 18:15:02 -0400

    Initial Commit

---
+++ Swift_Final_Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Final_Project
+//
+//  Created by MacStudent on 2019-10-15.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
