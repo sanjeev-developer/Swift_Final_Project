a91f6e62fc8123541e2fe026271b0a165929b18e
sanjeev <sanjeevgupta9223@gmail.com>
2019-10-24 00:43:26 -0400

    commit 50

--- Swift_Final_Project/Employee/Intern.swift
+++ Swift_Final_Project/Employee/Intern.swift
@@ -18,6 +18,7 @@ class Intern : Employee
     init( id : Int,type: String,name : String , age : Int, schoolName:String, employeeType:String, vobj: Model.Vehicle)
     {
         
+        
         self.schoolName = schoolName
         super.init(name: name, id: id, age: age, vobj: vobj, type: type)
        
