ecf4108aff9e122d566fa2fef0c1b95ff833b738
Vivek <madishetty.vivek@gmail.com>
2019-10-24 01:15:47 -0400

    motorcycle class commit

--- Swift_Final_Project/Vehicle/Motorcycle.swift
+++ Swift_Final_Project/Vehicle/Motorcycle.swift
@@ -7,26 +7,4 @@
 //
 
 import Foundation
-class Motorcycle : Vehicles
-{
-    
-    
-    var model : String = ""
-    var insurance = Bool()
-    var yearOfMake = String()
-    
-    init(make : String , plate : String, model : String, insurance : Bool , year: String)
-    {
-        super.init(make: make, plate: plate)
-        self.model = model
-        self.yearOfMake = year
-        self.insurance = insurance
-        
-    }
-    override func displayData() {
-        super.displayData()
-        print("Model : \(model)")
-        print("Year of Make : \(yearOfMake)")
-        print("Insurance : \(insurance)")
-    }
-}
+
